3551d49683edd20ecb800e0095e866534c1bdcb5
Ben Tupper <btupper@bigelow.org>
2021-09-17 08:46:49 -0400

    move 3step items into 3step folder

---
+++ 3step/asv_18S_postprocess.R
@@ -0,0 +1,192 @@
+library(dplyr)
+library(readr)
+
+library(ShortRead)  
+library(Biostrings)
+library(dada2)
+
+library(charlier)
+library(dadautils)
+
+
+#' main processing step - tries to operate as a pipeline returning 0 (success) or
+#' failure ( > 0)
+#' 
+#' @param CFG list, configuration from a config file
+#' @return integer where 0 means success
+main <- function(CFG){
+  
+  RETURN <- 0
+  
+  if (tolower(CFG$stage) != "postprocess"){
+    warning("configuration stage should be 'postprocess'")
+    return(RETURN + 1)
+  }
+  
+  if (CFG$multithread[1] == "auto") CFG$multithread <- charlier::count_cores()
+  
+  if (!dir.exists(CFG$input_path)){
+    warning("input path not found:", CFG$input_path)
+    return(RETURN + 1)
+  }
+  
+  if (!charlier::make_path(CFG$output_path)){
+    warning("output path not created:", CFG$output_path)
+    return(RETURN + 1)
+  }
+  
+  # logging - see https://github.com/BigelowLab/charlier/wiki/Logging
+  charlier::start_logger(filename = file.path(CFG$output_path, "log"))
+  
+  # audit - see https://github.com/BigelowLab/charlier/wiki/Auditing
+  PBS_JOBID <- charlier::get_pbs_jobid(no_pbs_text = "not in PBS queue")
+  charlier::audit(file.path(CFG$output_path, "audit_postsupervision.txt"), pbs_jobid = PBS_JOBID)
+  
+  # add baseline info into log, just because
+  charlier::info("**** starting run ****")
+  charlier::info("NCPUS: %s", as.character(CFG$multithread))
+  charlier::info("System PID: %s", Sys.getpid())
+  charlier::info("PBS_JOBID: %s", PBS_JOBID)
+  charlier::info("VERSION: %s", CFG$version)
+  charlier::info("INPUT PATH: %s", CFG$input_path)
+  charlier::info("OUTPUT PATH: %s", CFG$output_path)
+  
+  charlier::info("checking for input fastq files")
+  input_files <- dadautils::list_filepairs(CFG$input_path) %>%
+                   dadautils::verify_filepairs()
+
+  # pacbio?
+  norev <- (length(input_files) == 1) || (lengths(input_files)[[2]] == 0)
+ 
+  sample.names <- dadautils::extract_sample_names(input_files, rule="basename")
+  
+  
+  filter_trim_file <- file.path(CFG$output_path, "filter_and_trim.csv")
+  if (file.exists(filter_trim_file)){
+    charlier::info("read filter_and_trim")
+    filter_trim <- suppressMessages(readr::read_csv(filter_trim_file))
+  } else {
+    charlier::error("filter_and_trim file not found: %s", filter_trim_file)
+    stop("filter_and_trim file not found: ", filter_trim_file)
+  }
+  
+  
+  err_file <- file.path(CFG$output_path, "learn_errors")
+  if (file.exists(err_file)){
+    charlier::info("read learn errors")
+    err <- dadautils::read_errors(err_file)
+  } else {
+    charlier::error("learn_errors file not found: %s", err_file)
+    stop("learn_errors file not found: ", err_file)
+  }
+  
+  charlier::info("run dada")
+  dada_r <- dadautils::run_dada(input_files, err, 
+                                multithread = CFG$multithread,
+                                pool = CFG$dada2_dada$pool)
+  
+  # run merge pairs
+  charlier::info("merge pairs")
+  mergers <- dadautils::merge_pairs(input_files, dada_r, 
+                                    verbose = TRUE,
+                                    minOverlap = CFG$dada2_merge_pairs$minOverlap) %>%
+             dadautils::write_mergers(file.path(CFG$output_path, "mergers"))
+  
+  
+  charlier::info("make sequence table")
+  seqtab <- dada2::makeSequenceTable(mergers) 
+  tseqtab <- dplyr::as_tibble(t(seqtab)) %>%
+    readr::write_csv(file.path(CFG$output_path, "seqtab.csv"))
+  
+  charlier::info("remove Bimera Denovo")
+  seqtab.nochim <- dada2::removeBimeraDenovo(seqtab, 
+                                             method      = CFG$dada2_removeBimeraDenovo_seqtab$method, 
+                                             multithread = CFG$multithread, 
+                                             verbose     = CFG$dada2_removeBimeraDenovo_seqtab$verbose)
+  #tseqtab.nochim <- dplyr::as_tibble(t(seqtab.nochim)) %>%
+  #  readr::write_csv(file.path(CFG$output_path, "seqtab-nochim.csv"))
+  
+  charlier::info("write fasta")
+  fasta <- dadautils::asv_fasta(seqtab.nochim, file = file.path(CFG$output_path,"ASV_sequences.fasta"))
+  
+  tseqtab.nochim <- dplyr::as_tibble(t(seqtab.nochim)) %>%
+    dplyr::mutate(ASV = names(fasta)) %>%
+    dplyr::relocate(ASV, .before = 1) %>%
+    readr::write_csv(file.path(CFG$output_path, "seqtab-nochim.csv"))
+    
+  charlier::info("write track")
+  track <- dplyr::tibble(
+                         name               = sample.names,
+                         input              = filter_trim$reads.in, 
+                         filtered           = filter_trim$reads.out,
+                         denoised_forward   = sapply(dada_r$forward, dadautils::count_uniques), 
+                         denoised_reverse   = sapply(dada_r$reverse, dadautils::count_uniques), 
+                         merged             = sapply(mergers, dadautils::count_uniques), 
+                         nonchim            = rowSums(seqtab.nochim),
+                         final_prr          = nonchim/input) %>%
+    readr::write_csv(file.path(CFG$output_path, "track.csv"))
+  
+  
+  charlier::info("assign taxonomy")
+  taxa <- dadautils::assign_taxonomy(seqtab.nochim, 
+                                     refFasta          = CFG$dada2_assignTaxonomy_nochim$refFasta, 
+                                     taxLevels         = CFG$dada2_assignTaxonomy_nochim$taxLevels, 
+                                     minBoot           = CFG$dada2_assignTaxonomy_nochim$minBoot, 
+                                     outputBootstraps  = CFG$dada2_assignTaxonomy_nochim$outputBootstraps, 
+                                     verbose           = CFG$dada2_assignTaxonomy_nochim$verbose, 
+                                     multithread       = CFG$multithread,
+                                     drop_levels       = "NA",
+                                     save_file         = TRUE,
+                                     filename          = file.path(CFG$output_path, "taxa.csv"))
+  
+  charlier::info("writing ASV_taxa")
+  ttaxa <- dplyr::as_tibble(taxa) %>%
+    dplyr::mutate(ASV = names(fasta)) %>%
+    dplyr::relocate(ASV, .before = 1) %>%
+    readr::write_csv(file.path(CFG$output_path, "ASV_taxa.csv"))
+
+  
+  if ("dada2_addSpecies" %in% names(CFG)){
+    charlier::info("add species to taxonomy")
+    if (length(taxa) == 2 && "tax" %in% names(taxa)){
+      taxa <- taxa$tax
+    }
+    taxa <- dada2::addSpecies(taxa, refFasta = CFG$dada2_addSpecies$refFasta)
+    readr::write_csv(taxa %>% dplyr::as_tibble(), 
+                     file.path(CFG$output_path, "taxa-species.csv"))
+  }
+  
+  if ("dada2_taxa_remove" %in% names(CFG)){
+    charlier::info("remove unwanted values in taxonomy")
+    taxa <- taxa %>%
+      dadautils::taxa_remove(vars = CFG$dada2_taxa_remove)
+    readr::write_csv(taxa %>% dplyr::as_tibble(), 
+                     file.path(CFG$output_path, "taxa-cleaned.csv"))
+  }
+  
+  
+  CFG$stage <- "dada-complete"
+  charlier::write_config(CFG, file.path(CFG$output_path, paste0("complete-", basename(CFGFILE))))
+  charlier::info("done: %s", CFG$output_path)   
+  return(RETURN)
+} #main
+
+
+# we only run is run as a script - not if interactive
+if (!interactive()){
+  CFGFILE <- commandArgs(trailingOnly = TRUE)[1]
+} else {
+  CFGFILE <- "/mnt/storage/data/edna/dada/projects/3step/18S/asv_18S_postprocess.yaml"
+}
+
+CFG <- charlier::read_config(CFGFILE[1], 
+                             autopopulate = TRUE,
+                             fields = list(
+                             data_path = "data_path",
+                             reference_path = "reference_path"),
+                             rootname = "global")
+
+if (!interactive()){
+  ok <- main(CFG)
+  quit(save = "no", status = ok)
+}
---
+++ 3step/asv_18S_preprocess.R
@@ -0,0 +1,202 @@
+library(dplyr)
+library(readr)
+
+library(ShortRead)  
+library(Biostrings)
+library(dada2)
+
+library(charlier)
+library(dadautils)
+
+
+#' main processing step - tries to operate as a pipeline returning 0 (success) or
+#' failure ( > 0)
+#' 
+#' @param CFG list, configuration from a config file
+#' @return integer where 0 means success
+main <- function(CFG){
+  
+  RETURN <- 0
+  
+  if (tolower(CFG$stage) != "preprocess"){
+    warning("configuration stage should be 'preprocess'")
+    return(RETURN + 1)
+  }
+  
+  if (CFG$multithread[1] == "auto") CFG$multithread <- charlier::count_cores()
+  
+  if (!dir.exists(CFG$input_path)){
+    warning("input path not found:", CFG$input_path)
+    return(RETURN + 1)
+  }
+  
+  if (!charlier::make_path(CFG$output_path)){
+    warning("output path not created:", CFG$output_path)
+    return(RETURN + 1)
+  }
+  
+  # logging - see https://github.com/BigelowLab/charlier/wiki/Logging
+  charlier::start_logger(filename = file.path(CFG$output_path, "log"))
+  
+  # audit - see https://github.com/BigelowLab/charlier/wiki/Auditing
+  PBS_JOBID <- charlier::get_pbs_jobid(no_pbs_text = "not in PBS queue")
+  charlier::audit(file.path(CFG$output_path, "audit.txt"), pbs_jobid = PBS_JOBID)
+  
+  # add baseline info into log, just because
+  charlier::info("**** starting run ****")
+  charlier::info("NCPUS: %s", as.character(CFG$multithread))
+  charlier::info("System PID: %s", Sys.getpid())
+  charlier::info("PBS_JOBID: %s", PBS_JOBID)
+  charlier::info("VERSION: %s", CFG$version)
+  charlier::info("INPUT PATH: %s", CFG$input_path)
+  charlier::info("OUTPUT PATH: %s", CFG$output_path)
+  
+  charlier::info("checking for input fastq files")
+  input_files <- dadautils::list_filepairs(CFG$input_path) %>%
+                   dadautils::verify_filepairs()
+
+  # pacbio?
+  norev <- (length(input_files) == 1) || (lengths(input_files)[[2]] == 0)
+ 
+  sample.names <- dadautils::extract_sample_names(input_files, rule="basename")
+  
+  
+  # cutadapt?
+  if ("cutadapt" %in% names(CFG) && CFG$cutadapt$run){
+    charlier::info("run cutadapt on input files")
+    cutadapt_path <- file.path(CFG$output_path, "cutadapt")
+    ok <- charlier::make_path(cutadapt_path)
+    cut_files <- sapply(input_files,
+      function(ff){
+        file.path(cutadapt_path, basename(ff))  
+      }, simplify = FALSE)
+    ok <- dadautils::run_cutadapt(cut_files, input_files, CFG, save_graphics = FALSE)
+    if (all(ok == 0)) {
+      input_files <- cut_files
+    } else {
+      charlie::error("Something is wrong with cutadapt: %s", paste(ok, sep = ", "))
+      stop("what just happened to cutadapt?")
+    }
+    CFG$cutadapt$run <- FALSE
+  }
+  
+  
+  charlier::info("generate quality profiles")  
+  qpp <- quality_profile_pairs(input_files, 
+                               plot_filename=file.path(CFG$output_path, "quality_profiles.pdf"), 
+                               overlap_filename=file.path(CFG$output_path, "overlap.csv")) %>%
+                               dadautils::write_QPP(file.path(CFG$output_path, "qpp.rds"))
+  
+  # if the user provided explicit truncLen values we use those (this should be very rare or non-existent), if not then we extract those from qpp
+  if (inherits(CFG$dada2_filterAndTrim$truncLen, "character")){
+    if(CFG$dada2_filterAndTrim$truncLen[1] == "auto"){
+      charlier::info("using autocomputed truncLen")
+      truncLen <- dplyr::tibble(forward = qpp$forward$cutoff$Cycle,
+                                forward_file = input_files$forward)
+      if (!norev) truncLen <- dplyr::mutate(truncLen, 
+          reverse = qpp$reverse$cutoff$Cycle,
+          reverse_file = input_files$reverse)
+      truncLen <- readr::write_csv(truncLen, file.path(CFG$output_path, "truncLen.csv"))
+      CFG$dada2_filterAndTrim$truncLen <- file.path(CFG$output_path, "truncLen.csv")
+    } else if (file.exists(CFG$dada2_filterAndTrim$truncLen)){
+      charlier::info("reading truncLen from file: %s", CFG$dada2_filterAndTrim$truncLen)
+      truncLen <- readr::read_csv(CFG$dada2_filterAndTrim$truncLen)
+    } else {
+      charlier::error("truncLen can be character, bit if so must be 'auto' or a filename")
+      stop("truncLen not found:", CFG$dada2_filterAndTrim$truncLen)
+    }
+  } else {
+    charlier::info("using user provided truncLen vector: %s", paste(CFG$dada2_filterAndTrim$truncLen, sep = ", ") )
+    truncLen <- CFG$dada2_filterAndTrim$truncLen
+  }
+
+  charlier::info("estimate expected error thresholds (for maxEE)")
+  peet <- paired_quality_scores(input_files) %>%
+   paired_ee_per_read() %>%
+    paired_ee_threshold(sample_names  = sample.names, 
+                        filename = file.path(CFG$output_path, "EE_thresholds.csv"))
+  
+  
+  if(CFG$dada2_filterAndTrim$run){
+  
+    charlier::info("filter and trim of input files")
+    
+    filtN_path <- file.path(CFG$output_path, CFG$dada2_filterAndTrim$name)
+    if (!charlier::make_path(filtN_path)) {
+      charlier::error("filtN_path not created: %s", filtN_path)
+      return(RETURN + 1)  
+    }
+    
+    filtN_r <- dadautils::filter_and_trim(input_files, 
+                                          output_path  = filtN_path,
+                                          maxN         = CFG$dada2_filterAndTrim$maxN, 
+                                          multithread  = CFG$multithread, 
+                                          truncLen     = truncLen,
+                                          cutoff_params = CFG$dada2_filterAndTrim$cutoff_params,
+                                          minLen       = CFG$dada2_filterAndTrim$minLen,
+                                          maxEE        = CFG$dada2_filterAndTrim$maxEE,
+                                          truncQ       = CFG$dada2_filterAndTrim$truncQ,
+                                          rm.phix      = CFG$dada2_filterAndTrim$rm.phix,
+                                          compress     = CFG$dada2_filterAndTrim$compress,
+                                          verbose      = CFG$dada2_filterAndTrim$verbose,
+                                          save_results = FALSE) %>%
+                readr::write_csv(file.path(CFG$output_path, "filter_and_trim.csv"))
+    
+    filtN_files <- dadautils::list_filepairs(filtN_path) 
+    if(identical(unname(lengths(filtN_files)), c(0,0))){
+      stop("No filtN files produced")
+    }
+    if (!identical(lengths(input_files), lengths(filtN_files))){
+      # presumably filter_and_trim dropped some files if we get here... 
+      # so we need to trim input_files to match.  We assume that the output basenames are the same as the
+      # input basenames - so all we need to do is match
+      input_files <- sapply(names(input_files),
+        function(name){
+          ix <- basename(input_files[[name]]) %in% basename(filtN_files[[name]])
+          input_files[[name]][ix]
+       }, simplify = FALSE)
+      filtN_r <- filtN_r %>%
+             dplyr::filter(reads.out > 0)
+      sample.names <- dadautils::extract_sample_names(input_files, rule="basename")
+      
+      CFG$dada2_filterAndTrim$run <- FALSE
+      
+    } # check for dropped inputs
+  
+    if (CFG$dada2_learnErrors$run){
+      charlier::info("learn errors")
+      err <- dadautils::learn_errors(filtN_files,
+                                     output_path = CFG$output_path,
+                                     multithread = CFG$multithread,
+                                     save_output = FALSE, 
+                                     save_graphics = TRUE) %>%
+             dadautils::write_errors(file.path(CFG$output_path, "learn_errors"))
+      CFG$dada2_learnErrors$run <- FALSE
+    } # learn errors
+  } # filter and trim
+  
+  CFG$stage <- "supervision"
+  charlier::write_config(CFG, file.path(CFG$output_path, basename(CFGFILE)))
+  charlier::info("done: %s", CFG$output_path)   
+  return(RETURN)
+} #main
+
+
+# we only run is run as a script - not if interactive
+if (!interactive()){
+  CFGFILE <- commandArgs(trailingOnly = TRUE)[1]
+} else {
+  CFGFILE <- "/mnt/storage/data/edna/dada/projects/3step/18S/asv_18S_preprocess.yaml"
+}
+
+CFG <- charlier::read_config(CFGFILE[1], 
+                             autopopulate = TRUE,
+                             fields = list(
+                             data_path = "data_path",
+                             reference_path = "reference_path"),
+                             rootname = "global")
+
+if (!interactive()){
+  ok <- main(CFG)
+  quit(save = "no", status = ok)
+}
